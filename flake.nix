205e176eadcbc885f43e0b9c3e9d6b82c774e06e
jupblb <jupblb@google.com>
2025-05-21 12:05:56 +0200

    Add gtasks-md to flake outputs

--- flake.nix
+++ flake.nix
@@ -7,6 +7,13 @@
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        dependencies   = python3Packages: with python3Packages; [
+          google-api-python-client
+          google-auth-httplib2
+          google-auth-oauthlib
+          python-pandoc
+          xdg
+        ];
         pkgs           = (import nixpkgs) { inherit system; };
         python-pandoc  = with pkgs.python3Packages; buildPythonPackage rec {
           pname                 = "pandoc";
@@ -17,13 +24,7 @@
           };
           version               = "2.4";
         };
-        pythonWithPkgs = pkgs.python3.withPackages(p: with p; [
-          google-api-python-client
-          google-auth-httplib2
-          google-auth-oauthlib
-          python-pandoc
-          xdg
-        ]);
+        pythonWithPkgs = pkgs.python3.withPackages(dependencies);
       in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs;
@@ -31,6 +32,18 @@
           shellHook   = ''
             PYTHONPATH=${pythonWithPkgs}/${pythonWithPkgs.sitePackages}
           '';
+        };
+
+        packages = {
+          gtasks-md = pkgs.python3Packages.buildPythonApplication {
+            format                = "pyproject";
+            nativeBuildInputs     = with pkgs.python3Packages;
+              [ setuptools wheel ];
+            pname                 = "gtasks-md";
+            propagatedBuildInputs = dependencies(pkgs.python3Packages);
+            src                   = self;
+            version               = "0.0.10";
+          };
         };
       }
     );
